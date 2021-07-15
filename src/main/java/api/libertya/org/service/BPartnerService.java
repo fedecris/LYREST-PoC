354ad88da7289b2d56a0f9eb6ffef7bfbede6250
Federico Cristina <federicocristina@gmail.com>
2021-07-15 16:05:17 -0300

    Generalizacion para simplificar implementacion de nuevas entidades

--- src/main/java/api/libertya/org/service/BPartnerService.java
+++ src/main/java/api/libertya/org/service/BPartnerService.java
@@ -5,32 +5,11 @@ import org.openXpertya.model.MBPartner;
 import org.openXpertya.util.Env;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 public class BPartnerService extends GeneralService {
 
-
-    public MBPartner retrieveByID(int ID) throws IllegalArgumentException {
-        return loadBPartner(ID);
-    }
-
-    public String deleteByID(int ID) throws IllegalArgumentException, IllegalStateException {
-        return deletePO(loadBPartner(ID));
-    }
-
-    public String updateByID(int ID, Map<String, String> values) {
-        return savePO(setValues(loadBPartner(ID), values));
-    }
-
-    public String insert(Map<String, String> values) {
-        return savePO(setValues(loadBPartner(0), values));
-    }
-
-    protected MBPartner loadBPartner(int ID) {
+    protected MBPartner loadPOSpecific(int ID) {
         return (MBPartner)loadPO(ID, () -> new MBPartner(Env.getCtx(), ID, null));
     }
-
-
 
 }
--- src/main/java/api/libertya/org/service/GeneralService.java
+++ src/main/java/api/libertya/org/service/GeneralService.java
@@ -1,13 +1,43 @@
 package api.libertya.org.service;
 
 import api.libertya.org.common.POLoaderInterface;
+import org.openXpertya.model.MBPartner;
 import org.openXpertya.model.PO;
 import org.openXpertya.util.CLogger;
 
 import java.util.Map;
 
-public class GeneralService {
+public abstract class GeneralService {
 
+    /**
+     * Unico metodo a implementar en las subclases para CRUD de entidades
+     * @param ID identificador del objeto a recuperar/modificar/eliminar o 0 en caso de insercion
+     * @return un PO especifico, segun subclase por ejemplo MBPartner, MProduct, etc.
+     *          apoyandose en loadPO a fin de validar la existencia del objeto
+     */
+    protected abstract PO loadPOSpecific(int ID);
+
+    /** Recupera una entidad por su ID */
+    public PO retrieveByID(int ID) throws IllegalArgumentException {
+        return loadPOSpecific(ID);
+    }
+
+    /** Elimina una entidad por su ID */
+    public String deleteByID(int ID) throws IllegalArgumentException, IllegalStateException {
+        return deletePO(loadPOSpecific(ID));
+    }
+
+    /** Elimina una entidad por su ID */
+    public String updateByID(int ID, Map<String, String> values) {
+        return savePO(setValues(loadPOSpecific(ID), values));
+    }
+
+    /** Inserta una nueva entidad */
+    public String insert(Map<String, String> values) {
+        return savePO(setValues(loadPOSpecific(0), values));
+    }
+
+    /** Creacion / Recuperacion de un objeto segun su ID */
     protected PO loadPO(int ID, POLoaderInterface loader) throws IllegalArgumentException {
         PO po = loader.getPOInstance();
         /* ID es mayor a cero solo para updates */
@@ -17,21 +47,28 @@ public class GeneralService {
         return po;
     }
 
-    protected String savePO(PO po) throws IllegalStateException {
+    /** Persistir objeto
+     * @return ID del objeto insertado / actualizado si todo OK
+     */
+    private String savePO(PO po) throws IllegalStateException {
         if (!po.save()) {
             throw new IllegalStateException(String.format("Error updating: %s", CLogger.retrieveErrorAsString()));
         }
         return String.valueOf(po.getID());
     }
 
-    protected String deletePO(PO po) throws IllegalArgumentException, IllegalStateException {
+    /** Eliminar objeto
+     * @return ID del objeto eliminado si todo OK
+     */
+    private String deletePO(PO po) throws IllegalArgumentException, IllegalStateException {
         if (!po.delete(true)) {
             throw new IllegalStateException(String.format("Error deleting: %s", CLogger.retrieveErrorAsString()));
         }
         return String.valueOf(po.getID());
     }
 
-    protected PO setValues(PO po, Map<String, String> values) {
+    /** Asignacion de valores en un PO a partir del map recibido */
+    private PO setValues(PO po, Map<String, String> values) {
         values.keySet().forEach(
                 (k) -> po.set_Value(k, values.get(k))
         );
